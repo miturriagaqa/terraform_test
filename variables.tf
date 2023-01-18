c8cf9d95f2f88ff86661c01747879187622ad10c
miturriagaqa <109548388+miturriagaqa@users.noreply.github.com>
2023-01-18 15:26:55 -0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "access_key" {
+  description = "Value of the Access key for AWS"
+  type        = string
+}
+
+variable "secret_key" {
+  description = "Value of the Secret Key for AWS"
+  type        = string
+}
