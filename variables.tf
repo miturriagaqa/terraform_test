019ca67949385044e85295126db66ece4abe9551
miturriagaqa <109548388+miturriagaqa@users.noreply.github.com>
2023-05-15 14:21:57 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,7 +7,3 @@ variable "secret_key" {
   description = "Value of the Secret Key for AWS"
   type        = string
 }
-
-variable "key_name" {
-  description = "Value of the key pair"
-  type = string
