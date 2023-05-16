b6d4907c7ded13fe5966a383cad58103ec71badf
miturriagaqa <109548388+miturriagaqa@users.noreply.github.com>
2023-05-16 16:25:48 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -11,5 +11,5 @@ variable "secret_key" {
 variable "instance_name" {
   description = "Value of the Name tag for the EC2 instance"
   type        = string
-  default     = "AutomationAwsTerraform"
+  default     = "mishawsterraform"
 }
