ffbb83525005e88961d472d131ffcf5f02562e25
miturriagaqa <109548388+miturriagaqa@users.noreply.github.com>
2023-06-16 11:48:42 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -65,3 +65,15 @@ output "instance_public_ip" {
   value       = aws_instance.app_server.public_ip
 }
 
+output "private_ip" {
+  value = aws_instance.app_server.private_ip
+}
+
+output "vpc_security_group_ids" {
+  value = aws_instance.app_server.vpc_security_group_ids
+}
+
+output "availability_zone" {
+  value = aws_instance.app_server.availability_zone
+}
+
