8cf7d3b2434a0076c0896d0c97a4c43a1385888a
miturriagaqa <109548388+miturriagaqa@users.noreply.github.com>
2023-06-16 10:31:02 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -24,15 +24,16 @@ resource "aws_instance" "app_server"{
 
 output "app_server_ami_id" {
   value     = aws_instance.app_server.ami
+  sensitive = false
 }
 
 output "instance_name" {
   value     = aws_instance.app_server.tags.Name
 }
 
-output "app_server_instance_type" {
-  value     = aws_instance.app_server.instance_type
-}
+# output "app_server_instance_type" {
+#   value     = aws_instance.app_server.instance_type
+# }
 
 
 # Output hardcoded values
