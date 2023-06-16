9626ef1a9803bd1a9bce48eb07e0c5680e9ffa1a
miturriagaqa <109548388+miturriagaqa@users.noreply.github.com>
2023-06-16 17:20:14 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,13 +1,3 @@
-variable "access_key" {
-  description = "Value of the Access key for AWS"
-  type        = string
-}
-
-variable "secret_key" {
-  description = "Value of the Secret Key for AWS"
-  type        = string
-}
-
 variable "instance_name" {
   description = "Value of the Name tag for the EC2 instance"
   type        = string
