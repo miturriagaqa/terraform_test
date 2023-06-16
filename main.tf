9568e1589b9b9db2ee2b838e2c9c85cb6ea7dce7
miturriagaqa <109548388+miturriagaqa@users.noreply.github.com>
2023-06-16 13:24:43 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,6 +9,10 @@ terraform {
   required_version = ">= 0.13"
 }
 
+data "aws_key_pair" "existing_keypair" {
+  key_name = "mish_key_pair"
+}
+
 provider "aws" {
   region  = "us-east-2"
 }
@@ -16,6 +20,7 @@ provider "aws" {
 resource "aws_instance" "app_server"{
   ami           = "ami-0b301ce3ce347599c"
   instance_type = "t2.micro"
+  key_name = data.aws_key_pair.existing_keypair.key_name
   
   tags = {
     Name = var.instance_name
