7c4bf476ed3f8c174701ecff0f62c404e74cca22
miturriagaqa <109548388+miturriagaqa@users.noreply.github.com>
2023-06-16 17:19:41 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,42 +9,20 @@ terraform {
   required_version = ">= 0.13"
 }
 
+data "aws_key_pair" "existing_keypair" {
+  key_name = "mish_key_pair"
+}
+
 provider "aws" {
   region  = "us-east-2"
-  access_key = var.access_key
-  secret_key = var.secret_key
 }
 
 resource "aws_instance" "app_server"{
   ami           = "ami-0b301ce3ce347599c"
   instance_type = "t2.micro"
+  key_name = data.aws_key_pair.existing_keypair.key_name
   
   tags = {
     Name = var.instance_name
-  }
-}
-
-output "app_server_ami_id" {
-  value     = aws_instance.app_server.ami
-  sensitive = true
-}
-
-# Output hardcoded values
-output "message" {
-  value = "Hello, Terraform!"
-}
-
-output "number" {
-  value = 42
-}
-
-output "list" {
-  value = ["apple", "banana", "cherry"]
-}
-
-output "map" {
-  value = {
-    key1 = "value1"
-    key2 = "value2"
   }
 }
